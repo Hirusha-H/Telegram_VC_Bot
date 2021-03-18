73549c73e1f501f0ce1091e1d21031b4c6c2f637
Hirusha-H <77770753+Hirusha-H@users.noreply.github.com>
2021-03-18 12:25:25 +0530

    x

---
+++ config.py
@@ -0,0 +1,8 @@
+api_id = 2807289
+api_hash = "392602ecf5fd4cc741198b05c98d0ee4"
+sudo_chat_id = -1001233813450
+owner_id = 1340254734
+
+
+# Arq Api, don't make changes here
+ARQ_API = "https://thearq.tech"
